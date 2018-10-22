84ea69e6c67b6fc95ce597e15700283d460dc1ad
Suraj Budhathoki <sbudhathoki214@gmail.com>
2018-10-21 21:01:12 -0500

    added survey questions

--- app/data/employees.js
+++ app/data/employees.js
@@ -22,6 +22,102 @@ var employeesList = [
         "name" : "brad",
         "photo" : "\assets\images\brad.jpg",
         "score" : [
+            2,
+            4,
+            1,
+            3,
+            4,
+            2,
+            4,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        "name" : "charlie",
+        "photo" : "\assets\images\charlie.jpg",
+        "score" : [
+            2,
+            2,
+            3,
+            4,
+            4,
+            2,
+            1,
+            2,
+            3,
+            2
+        ]
+    },
+    {
+        "name" : "derek",
+        "photo" : "\assets\images\derek.jpg",
+        "score" : [
+            5,
+            4,
+            3,
+            4,
+            5,
+            5,
+            4,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        "name" : "emily",
+        "photo" : "\assets\images\emily.jpg",
+        "score" : [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name" : "felicia",
+        "photo" : "\assets\images\felicia.jpg",
+        "score" : [
+            4,
+            5,
+            4,
+            5,
+            2,
+            1,
+            2,
+            2,
+            1,
+            1
+        ]
+    },
+    {
+        "name" : "george",
+        "photo" : "\assets\images\george.jpg",
+        "score" : [
+            1,
+            2,
+            1,
+            2,
+            3,
+            2,
+            2,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        "name" : "holly",
+        "photo" : "\assets\images\holly.jpg",
+        "score" : [
             5,
             1,
             4,
@@ -35,19 +131,35 @@ var employeesList = [
         ]
     },
     {
-        "name" : "charlie",
-        "photo" : "\assets\images\charlie.jpg",
+        "name" : "isha",
+        "photo" : "\assets\images\isha.jpg",
         "score" : [
             5,
-            1,
+            3,
             4,
-            4,
-            5,
             1,
             2,
-            5,
+            1,
             4,
-            1
+            3,
+            2,
+            5
+        ]
+    },
+    {
+        "name" : "julia",
+        "photo" : "\assets\images\julia.jpg",
+        "score" : [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     }
 
